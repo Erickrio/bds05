0219d71f767e1286d4f9e7d69c8518d3ed700f91
Erick Costa Monteiro <erick.monteiro@unigranrio.br>
2022-11-29 10:51:20 -0300

    finish structure of project

---
+++ src/main/java/com/devsuperior/movieflix/services/AuthService.java
@@ -0,0 +1,37 @@
+package com.devsuperior.movieflix.services;
+
+
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.UserRepository;
+import com.devsuperior.movieflix.services.exceptions.ForbiddenException;
+import com.devsuperior.movieflix.services.exceptions.UnauthorizedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //retorna o usuario autenticado:
+    @Transactional(readOnly = true) //Diz ao BD que é somente leitura e evita lock
+    public User Authenticated() {
+        try {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            return userRepository.findByEmail(username);
+        } catch (Exception e) {
+            throw  new UnauthorizedException("Invalid User");
+        }
+    }
+
+    public void validateSelfOrAdmin(Long userId){
+        User user = Authenticated();//pega usuario autenticado
+        //Testa se não for o proprio usuario e não for o admin
+        if (!user.getId().equals(userId) && !user.hasHole("ROLE_ADMIN")){
+            throw new ForbiddenException("Acces danied");
+        }
+    }
+}
